5869cca2bdda869c12ce2fed61c69fcf07b0c9ef
root <root@cdumitri2.mylabserver.com>
2017-07-29 18:31:01 +0000

    add search in menu

--- adress_book.sh
+++ adress_book.sh
@@ -1,17 +1,78 @@
 #!/bin/bash
 
 
+delete(){
+    read -p "Enter name: " name
+    read -p "Enter surname: " surname
+    echo " $name $surname will be deleted; Are you use [Y/N]"
+    read -p "Please enter your choise Y or N " choose
+    if [ $choose == "Y" ] 
+    then
+       sed -i  "/$name/{/$surname/d}" data.txt
+    else
+       pause
+    fi
+}
+
+
+search_by_email(){
+     read -p "Enter the email adress: " email
+     grep $email data.txt | column -t -s ';'
+     pause
+}
+
+
+search_by_phone(){
+     read -p "Enter the phone number: " phone
+     grep $phone data.txt | column -t -s ';'
+     pause
+}
+
+search_by_surname(){
+     read -p "Enter the surname: " surname
+     grep $surname data.txt | column -t -s ';'
+     pause
+}
+
+
+search_by_name(){
+     read -p "Enter the name: " name
+     grep $name data.txt | column -t -s ';'
+     pause
+}
+
+
+search(){
+     clear
+     echo "1. Search by name"
+     echo "2. Search by surname"
+     echo "3. Search by phone number"
+     echo "4. Serach by email adress"
+     echo "5. Exit"
+     local choise
+     read -p "Please choose search criteria [1-4]: " choise
+     case $choise in
+          1) search_by_name;;
+          2) search_by_surname;;
+          3) search_by_phone;;
+          4) search_by_email;;
+          5) exit 0 ;;
+          *) echo -e "Choise is not avaible"
+     esac      
+}
 
 add(){
-    read -p "Enter name and surname: " name
+    read -p "Enter the name: " name
+    read -p "Enter the surname: " surname
     read -p "Enter phone number:  " phone
     read -p "Enter email adress: " email
     echo "Datele introduse sunt:"
-    echo "Name and surname: $name"
+    echo "Name: $name"
+    echo "Surname: $surname"
     echo "Phone number: $phone"
     echo "Email adress: $email"
    # echo "Name and surname;Phone number;Email adress" >> data.txt
-    echo "$name;$phone;$email" >> data.txt
+    echo "$name;$surname;$phone;$email" >> data.txt
     pause
 }
 
